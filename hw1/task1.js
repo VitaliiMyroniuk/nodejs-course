5020ce8fddc239434cee0a7d34d2b9f1f8e83d3d
Vitalii <vitalii@EPUAKYIW1370.kyiv.epam.com>
2020-02-15 15:30:24 +0200

    Add Task 1.1
    Read a string from the standard input stdin, reverse it and then write it to the standard output stdout.

---
+++ hw1/task1.js
@@ -0,0 +1,27 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+function reverseString(str) {
+    return [...str].reverse().join('');
+}
+
+function run1() {
+    rl.on('close', () => rl.close());
+    rl.on('line', (input) => console.log(reverseString(input)));
+}
+
+// Alternative solution
+function run2() {
+    process.stdin.on('data', data => {
+        let input = data.toString().replace('\n', '');
+        process.stdout.write(reverseString(input) + '\n');
+    });
+}
+
+run1();
+// run2();
